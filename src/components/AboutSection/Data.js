1ea92cb1cdb11f70f585a914c498ac357dee4a9b
BlueFlaw <allard_lavaritte@icloud.com>
2022-05-24 15:00:31 +0800

    clean-up about section

--- src/components/AboutSection/Data.js
+++ src/components/AboutSection/Data.js
@@ -1,30 +1,3 @@
-export const About1 = {
-    lightBg: true,
-    lightText: false,
-    topLine: 'this website is created using',
-    headline: 'React',
-    description: 'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes. Declarative views make your code more predictable and easier to debug.',
-    buttonLabel: 'Get Started',
-    imgStart: true,
-    img: require('../../images/react.svg'),
-    alt: 'alt',
-    pathLink: "//reactjs.org/docs/getting-started.html",
-    dark: true
-}
-export const About2 = {
-    lightBg: false,
-    lightText: true,
-    topLine: 'download my code',
-    headline: 'GitHub',
-    description: 'The "clone" command downloads an existing Git repository to your local computer. You will then have a full-blown, local version of that Git repo and can start working on the project.',
-    buttonLabel: 'View on GitHub',
-    imgStart : false,
-    img: require('../../images/git.svg'),
-    alt: 'alt',
-    pathLink: "https://github.com/blueflaw/lavaritte-website",
-    dark: false
-}
-
 export const About3 = {
     lightBg: true,
     lightText: false,
